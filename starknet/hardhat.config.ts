71459821bc5fd1f42a0363c731d21abc96e9eb87
pscott <scott.piriou@ledger.fr>
2021-11-16 11:41:55 +0100

    Update hardhat config to v0.5.2

--- starknet/hardhat.config.ts
+++ starknet/hardhat.config.ts
@@ -2,6 +2,9 @@ import '@shardlabs/starknet-hardhat-plugin';
 
 module.exports = {
   solidity: "0.8.4",
+  cairo: {
+    version: "0.5.2",
+  },
   networks: {
     myNetwork: {
       url: "http://starknet-devnet:5000"
